fd020d3826b04ef245e52e6d9f962943b5db6f68
zs <Z>
2020-11-24 23:35:17 +0200

    Added db

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ const express= require('express');
 const app = express();
 app.listen(process.env.PORT || 3000, process.env.HOST || 'localhost');
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
+
 app.get('/', function (req, res) {
-    res.send('Hello from workflow!')
+    res.send(process.env.DATABASE_URL)
   })
